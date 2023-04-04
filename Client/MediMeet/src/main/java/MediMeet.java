887f2c6aa82a7642e78568bf99031fe7d45ec604
Hasaan Ahmad <hasaanahmad10023@gmail.com>
2023-04-04 17:02:45 +0500

    Tempelate Done

---
+++ Client/MediMeet/src/main/java/MediMeet.java
@@ -0,0 +1,16 @@
+package Client.MediMeet.src.main.java;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author GOOD WILL ALWAYS
+ */
+public class MediMeet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
